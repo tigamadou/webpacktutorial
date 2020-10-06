a8e671933149a43f8a660a4847d5851b62ecf43f
genzaraky <amadou140589@gmail.com>
2020-10-06 10:08:54 +0100

    image loading done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
 import './style.css';
+import Boss from './boss.jpg'
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
+
+    // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Boss;
+
+   element.appendChild(myIcon);
+
+
     return element;
   }
   
